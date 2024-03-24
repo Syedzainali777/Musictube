66ce259bf9e341c2ee9ccc6750da13c355d0fc77
Syedzainali777 <ali4205359@gmail.com>
2024-03-24 19:08:41 +0500

    Utlis function for api response and error

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
-connectDB();
+  path: "./env",
+});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`App is listening of Port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo DB connection failed", error);
+    throw err;
+  });
 
 // import { Express } from "express";
 // const app = express();
--- src/utlis/ApiError.js
+++ src/utlis/ApiError.js
@@ -1,4 +1,4 @@
-import { Express } from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -11,9 +11,9 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: "16KB" }));
-app.use(express.unlencoded({ extended: "true", limit: "16KB" }));
-app.use(express.static("Public"));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 app.use(cookieParser());
 
 export { app };
